7b91dc0fe855418104494e6c24609f76bc60e360
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2024-11-27 11:42:49 +0900

    fix

---
+++ firmware/option（trrs)/cool642/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 m.ki (@telzo2000)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x33, 0x5C, 0xEE, 0xA1, 0x98, 0xA2, 0x87, 0x75}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
