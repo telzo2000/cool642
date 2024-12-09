5beda2ca4024242f1d25184fdc8d0f35dbc3b4b6
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2024-12-09 19:49:38 +0900

    fix

--- firmware/option_trrs/cool642/keymaps/vial/keymap.c
+++ firmware/option_trrs/cool642/keymaps/vial/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_ENT,
         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_RSFT,
         KC_ESC, KC_TAB, KC_LCTL,KC_LGUI,KC_SPC,KC_SPC, KC_SPC, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT, KC_BSPC
-    ), 
+    ),
      [3] = LAYOUT_split_3x6_3(
         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
         KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_ENT,
